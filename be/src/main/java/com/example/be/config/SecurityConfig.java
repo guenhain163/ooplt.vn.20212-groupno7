00200dbbb0b7d9fc39b681d6edd5bb49ed32ef66
guenhain163 <guenhain.163@gmail.com>
2022-07-08 18:35:49 +0700

    fix cors

--- be/src/main/java/com/example/be/config/SecurityConfig.java
+++ be/src/main/java/com/example/be/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.cors().and().csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.exceptionHandling().authenticationEntryPoint(
